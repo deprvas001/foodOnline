732a77c121cd3ee205459497b87c1d83163aff5b
Deepak Sharma <deepak.sharma@staqo.com>
2023-02-27 15:35:25 +0530

    menu builder

--- vendor/views.py
+++ vendor/views.py
@@ -8,7 +8,11 @@ from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.views import check_role_vendor
+from menu.models import Category,FoodItem
 
+def get_vendor(request):
+    vendor = Vendor.objects.get(user=request.user)
+    return vendor
 
 # Create your views here.
 @login_required(login_url='login')
@@ -40,3 +44,27 @@ def vprofile(request):
 
     }
     return render(request, 'vendor/vprofile.html', context)
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def menu_builder(request):
+    vendor = get_vendor(request)
+    categories = Category.objects.filter(vendor=vendor)
+    context = {
+        'categories':categories,
+    }
+    return render(request, 'vendor/menu_builder.html',context)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def fooditems_by_category(request,pk=None):
+    vendor = get_vendor(request)
+    category = get_object_or_404(Category, pk=pk)
+    fooditems = FoodItem.objects.filter(vendor=vendor, category=category)
+    print(fooditems)
+    context = {
+        'fooditems': fooditems,
+        'category': category,
+    }
+    return render(request, 'vendor/fooditems_by_category.html',context)
