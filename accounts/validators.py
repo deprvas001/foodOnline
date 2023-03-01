267b56501b94d73ccf41bb8a52821f9c483025bf
Deepak Sharma <deepak.sharma@staqo.com>
2023-03-01 13:06:17 +0530

    Add Food Item

--- accounts/validators.py
+++ accounts/validators.py
@@ -5,7 +5,7 @@ def allow_only_images_validator(value):
     ext = os.path.split(value.name)[1] #cover-image.png
     print(ext)
     valid_extensions = ['.png', '.jpg', '.jpeg']
-    if not ext.lower() in valid_extensions:
+    if  ext.lower() in valid_extensions:
         raise ValidationError("Unsupported file extension. Allowed extensions: "+ str(valid_extensions))
 
         
