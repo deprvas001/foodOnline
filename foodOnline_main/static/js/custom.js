424f3436d7f4fb43b623094c942ea40ef477302e
Deepak Sharma <deepak.sharma@staqo.com>
2023-03-27 14:59:37 +0530

    add to cart functionality

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -104,6 +104,13 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+
+                    // subtotal, tax and grandtotal
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total']
+                    )
                 }
             }
         })
@@ -123,6 +130,7 @@ $(document).ready(function(){
 
         food_id = $(this).attr('data-id');
         url = $(this).attr('data-url');
+        cart_id = $(this).attr('id');
 
         $.ajax({
             type:'GET',
@@ -138,6 +146,20 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+
+                      // subtotal, tax and grandtotal
+                      applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total']
+                    )
+
+                    if(window.location.pathname == '/cart/'){
+                        removeCartItem(response.qty, cart_id);
+                        checkEmptyCart();
+                    }
+
+                  
                 }
                 
             }
@@ -145,4 +167,56 @@ $(document).ready(function(){
 
     })
 
+
+    //delete cart
+    $('.delete_cart').on('click', function(e){
+        
+        e.preventDefault();
+
+        cart_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+       
+        $.ajax({
+            type:'GET',
+            url: url,
+            success: function(response){
+                console.log(response);
+            if(response.status == 'Failed'){
+                    swal(response.message, '', 'error')
+            }
+            else{
+                    $('#cart_counter').html(response.cart_counter['cart_count']);
+                    swal(response.status, response.message, "success")
+                    removeCartItem(0, cart_id)
+                    checkEmptyCart();
+             }
+                
+            }
+        })
+
+    })
+
+    // delete the cart element if the qty is 0
+    function removeCartItem(cartItemQty, cart_id){
+       if(cartItemQty <=0){
+        //remove the cart item element
+        document.getElementById("cart-item-"+cart_id).remove()
+       }
+    }
+
+    // Check if the cart is empty
+    function checkEmptyCart(){
+        var cart_counter = document.getElementById('cart_counter').innerHTML
+        if(cart_counter == 0){
+            document.getElementById("empty-cart").style.display = "block";
+        }
+    }
+
+    //apply cart amount
+    function applyCartAmounts(subtotal, tax, grand_total){
+        if(window.location.pathname="/cart/")
+        $('#subtotal').html(subtotal)
+        $('#tax').html(tax)
+        $('#total').html(grand_total)
+    }
 });
--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -14,3 +14,20 @@ def get_cart_counter(request):
         except:
             cart_count = 0
     return dict(cart_count=cart_count)
+
+
+def get_cart_amounts(request):
+    subtotal = 0
+    tax = 0
+    grand_total = 0 
+
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+        for item in cart_items:
+            fooditem = FoodItem.objects.get(pk=item.fooditem.id)
+            subtotal += (fooditem.price * item.quantity)
+        
+        grand_total = subtotal + tax
+    print(grand_total) 
+    print(subtotal) 
+    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
