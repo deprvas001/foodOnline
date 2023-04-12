ff7baac42a6d3cd7b4d41bd6c3953a1a4b6a2fdd
Deepak Sharma <deepak.sharma@staqo.com>
2023-04-12 10:48:00 +0530

    tax feature

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -108,9 +108,10 @@ $(document).ready(function(){
                     // subtotal, tax and grandtotal
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
+                    console.log(tax_dict)
                 }
             }
         })
@@ -150,7 +151,7 @@ $(document).ready(function(){
                       // subtotal, tax and grandtotal
                       applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -187,6 +188,12 @@ $(document).ready(function(){
             else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     swal(response.status, response.message, "success")
+
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax_dict'],
+                        response.cart_amount['grand_total']
+                    )
                     removeCartItem(0, cart_id)
                     checkEmptyCart();
              }
@@ -216,8 +223,16 @@ $(document).ready(function(){
     function applyCartAmounts(subtotal, tax, grand_total){
         if(window.location.pathname="/cart/")
         $('#subtotal').html(subtotal)
-        $('#tax').html(tax)
         $('#total').html(grand_total)
+
+        console.log(tax_dict)
+        for(key1 in tax_dict){
+            console.log(tax_dict[key1])
+            for(key2 in tax_dict[key1]){
+               // console.log(tax_dict[key1][key2])
+               $('#tax-'+key1).html(tax_dict[key1][key2])
+            }
+        }
     }
 
     $('.add_hour').on('click', function(e){
