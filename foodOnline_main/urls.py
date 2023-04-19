5ba0025b3997528a0d2ce2f0c5f5654befaf1082
Deepak Sharma <deepak.sharma@staqo.com>
2023-04-19 17:04:26 +0530

    checkout page

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
     path("cart/", MarketplaceViews.cart, name="cart"),
     path("search/", MarketplaceViews.search, name='search'),
 
+    #checkout
+    path('checkout/', MarketplaceViews.checkout, name='checkout'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
